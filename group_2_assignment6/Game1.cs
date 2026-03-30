805b6ed319a38ed8144858ba227b096869a8b83e
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2026-03-29 21:33:08 -0500

    added repaly bar and legend

--- group_2_assignment6/Game1.cs
+++ group_2_assignment6/Game1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -10,6 +10,10 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Grid _grid;
     private Texture2D _pixel;
+    private Texture2D _healthySprite;
+    private Texture2D _sickSprite;
+    private Texture2D _recoveredSprite;
+    private SpriteFont _font;
 
     private const int CellSize = 10;
     private const int GridRows = 60;
@@ -18,10 +22,19 @@ public class Game1 : Game
     private Simulation _simulation;
     private const float InfectionChance = 0.01f;
 
+    // UI panel
+    private const int PanelWidth = 160;
+    private int _gridPixelWidth;
+
+    // Replay button
+    private Rectangle _replayButtonRect;
+    private MouseState _previousMouse;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
-        _graphics.PreferredBackBufferWidth = GridCols * CellSize;
+        _gridPixelWidth = GridCols * CellSize;
+        _graphics.PreferredBackBufferWidth = _gridPixelWidth + PanelWidth;
         _graphics.PreferredBackBufferHeight = GridRows * CellSize;
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
@@ -31,6 +44,7 @@ public class Game1 : Game
     {
         _grid = new Grid(GridRows, GridCols, InitialInfectionRate);
         _simulation = new Simulation(_grid, InfectionChance);
+        _replayButtonRect = new Rectangle(_gridPixelWidth + 40, 15, 80, 35);
         base.Initialize();
     }
 
@@ -41,6 +55,11 @@ public class Game1 : Game
         // Create a 1x1 white pixel texture for drawing cells
         _pixel = new Texture2D(GraphicsDevice, 1, 1);
         _pixel.SetData(new[] { Color.White });
+
+        _healthySprite = Content.Load<Texture2D>("healthy");
+        _sickSprite = Content.Load<Texture2D>("sick");
+        _recoveredSprite = Content.Load<Texture2D>("recovered");
+        _font = Content.Load<SpriteFont>("DefaultFont");
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,6 +67,17 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        // Check for replay button click
+        MouseState mouse = Mouse.GetState();
+        if (mouse.LeftButton == ButtonState.Pressed &&
+            _previousMouse.LeftButton == ButtonState.Released &&
+            _replayButtonRect.Contains(mouse.Position))
+        {
+            _grid = new Grid(GridRows, GridCols, InitialInfectionRate);
+            _simulation = new Simulation(_grid, InfectionChance);
+        }
+        _previousMouse = mouse;
 
         _simulation.Update(gameTime);
         _grid.Update(gameTime);
@@ -59,21 +89,16 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.DarkBlue);
 
-        _spriteBatch.Begin();
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
+        // --- Draw grid ---
         for (int r = 0; r < _grid.Rows; r++)
         {
             for (int c = 0; c < _grid.Cols; c++)
             {
-                // Water cells - draw ocean
+                // Water cells
                 if (!_grid.IsLand(r, c))
-                {
-                    _spriteBatch.Draw(
-                        _pixel,
-                        new Rectangle(c * CellSize, r * CellSize, CellSize, CellSize),
-                        Color.DarkBlue);
                     continue;
-                }
 
                 Person person = _grid.GetCell(r, c);
                 Color color = person.Health switch
@@ -97,6 +122,62 @@ public class Game1 : Game
                     color);
             }
         }
+
+        // --- Draw UI panel ---
+        int panelX = _gridPixelWidth;
+        int panelH = GridRows * CellSize;
+
+        // Panel background
+        _spriteBatch.Draw(_pixel, new Rectangle(panelX, 0, PanelWidth, panelH), new Color(30, 30, 30));
+
+        // Replay button
+        Color btnColor = _replayButtonRect.Contains(Mouse.GetState().Position)
+            ? new Color(80, 80, 80)
+            : new Color(60, 60, 60);
+        _spriteBatch.Draw(_pixel, _replayButtonRect, btnColor);
+
+        // Button border
+        _spriteBatch.Draw(_pixel, new Rectangle(_replayButtonRect.X, _replayButtonRect.Y, _replayButtonRect.Width, 2), Color.White);
+        _spriteBatch.Draw(_pixel, new Rectangle(_replayButtonRect.X, _replayButtonRect.Bottom - 2, _replayButtonRect.Width, 2), Color.White);
+        _spriteBatch.Draw(_pixel, new Rectangle(_replayButtonRect.X, _replayButtonRect.Y, 2, _replayButtonRect.Height), Color.White);
+        _spriteBatch.Draw(_pixel, new Rectangle(_replayButtonRect.Right - 2, _replayButtonRect.Y, 2, _replayButtonRect.Height), Color.White);
+
+        // Button text
+        string replayText = "Replay";
+        Vector2 textSize = _font.MeasureString(replayText);
+        Vector2 btnTextPos = new Vector2(
+            _replayButtonRect.X + (_replayButtonRect.Width - textSize.X) / 2,
+            _replayButtonRect.Y + (_replayButtonRect.Height - textSize.Y) / 2);
+        _spriteBatch.DrawString(_font, replayText, btnTextPos, Color.White);
+
+        // --- Legend entries ---
+        int legendY = 70;
+        int spriteDisplaySize = 64;
+        int spriteCenterX = panelX + (PanelWidth - spriteDisplaySize) / 2;
+        int spacing = 100;
+
+        // Healthy
+        _spriteBatch.Draw(_healthySprite, new Rectangle(spriteCenterX, legendY, spriteDisplaySize, spriteDisplaySize), Color.White);
+        string healthyLabel = "Healthy";
+        Vector2 healthySize = _font.MeasureString(healthyLabel);
+        _spriteBatch.DrawString(_font, healthyLabel,
+            new Vector2(panelX + (PanelWidth - healthySize.X) / 2, legendY + spriteDisplaySize + 4), Color.Green);
+
+        // Sick
+        int sickY = legendY + spacing;
+        _spriteBatch.Draw(_sickSprite, new Rectangle(spriteCenterX, sickY, spriteDisplaySize, spriteDisplaySize), Color.White);
+        string sickLabel = "Sick";
+        Vector2 sickSize = _font.MeasureString(sickLabel);
+        _spriteBatch.DrawString(_font, sickLabel,
+            new Vector2(panelX + (PanelWidth - sickSize.X) / 2, sickY + spriteDisplaySize + 4), Color.Red);
+
+        // Recovered
+        int recY = legendY + spacing * 2;
+        _spriteBatch.Draw(_recoveredSprite, new Rectangle(spriteCenterX, recY, spriteDisplaySize, spriteDisplaySize), Color.White);
+        string recLabel = "Recovered";
+        Vector2 recSize = _font.MeasureString(recLabel);
+        _spriteBatch.DrawString(_font, recLabel,
+            new Vector2(panelX + (PanelWidth - recSize.X) / 2, recY + spriteDisplaySize + 4), Color.CornflowerBlue);
 
         _spriteBatch.End();
 
